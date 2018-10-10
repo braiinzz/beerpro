b882e9f34fde67a4faf045884b2deb160649ba22
Manuel Habert <manuel@habert.ch>
2018-10-10 09:41:55 +0200

    refactoring

--- app/src/main/java/ch/beerpro/presentation/profile/myfridge/FridgeViewModel.java
+++ app/src/main/java/ch/beerpro/presentation/profile/myfridge/FridgeViewModel.java
@@ -2,8 +2,6 @@ package ch.beerpro.presentation.profile.myfridge;
 
 import android.util.Pair;
 
-import org.apache.commons.lang3.NotImplementedException;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +10,6 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import ch.beerpro.data.repositories.BeersRepository;
 import ch.beerpro.data.repositories.CurrentUser;
-import ch.beerpro.data.repositories.WishlistRepository;
 import ch.beerpro.domain.models.Beer;
 import ch.beerpro.domain.models.FridgeItem;
 
@@ -20,10 +17,8 @@ import static androidx.lifecycle.Transformations.map;
 
 public class FridgeViewModel extends ViewModel implements CurrentUser {
     private final MutableLiveData<String> currentUserId = new MutableLiveData<>();
-    private final WishlistRepository wishlistRepository;
     private final BeersRepository beersRepository;
     public FridgeViewModel() {
-        wishlistRepository = new WishlistRepository();
         beersRepository = new BeersRepository();
         currentUserId.setValue(getCurrentUser().getUid());
     }
